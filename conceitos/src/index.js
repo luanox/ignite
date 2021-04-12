ab3bb8b71bb7e42c8c80733b34a0acd23cc91b0a
Luan Neves <luanuemg@hotmail.com>
2021-04-12 20:49:56 -0300

    add nodemon

--- conceitos/src/index.js
+++ conceitos/src/index.js
@@ -5,7 +5,7 @@ const app = express()
 // app.json()
 
 app.get("/", (req, res) => {
-    return res.json({message: "Hellooo"})
+    return res.json({message: "Hellooo luan"})
 })
 
 app.listen(3333)
