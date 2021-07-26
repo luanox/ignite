7e6b111332a9f844f2d0b8ead4f421f60062c9b2
Luan Neves <luanuemg@hotmail.com>
2021-07-26 18:30:35 -0300

    feat(account): created route statement

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -23,4 +23,16 @@ app.post("/account",(req, res) => {
   return res.status(201).json(customers)
 })
 
+app.get("/statement", (req, res) => {
+    const {cpf} = req.headers
+
+    const customer = customers.find((customer) => customer.cpf === cpf)
+
+    if(!customer){
+        return res.status(400).json({error: "Customer not found!!"})
+    }
+
+    return res.json(customer.statement)
+})
+
 app.listen(3333)
