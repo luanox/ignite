300ab720204b0ebb6226e81a0241cdbff60e1dd2
Luan Neves <luanuemg@hotmail.com>
2021-07-26 19:16:32 -0300

    feat(account): created route put and delete

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -19,6 +19,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
     return next()
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount
+        }else{
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
+
 // Routes
 app.post("/account",(req, res) => {
   const {cpf, name} = req.body
@@ -64,6 +76,56 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
     const {amount} = req.body
     const {customer} = req
 
+    const balance = getBalance(customer.statement)
+
+    if(balance < amount) {
+        return res.status(400).json({error: "Insufficient founds!!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return res.status(201).send()
+
+})
+
+app.get("/statement/date",verifyIfExistsAccountCPF, (req, res) => {
+    const {customer} = req
+    const {date} = req.query
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return res.json(statement)
+})
+
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const {name} = req.body
+    const {customer} = req
+
+    customer.name = name
+
+    return res.status(201).send()
+})
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const {customer} = req
+
+    return res.json(customer)
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const {customer} = req
+
+    customers.splice(customer, 1)
+
+    return res.status(20).json(customers)
 })
 
 app.listen(3333)
