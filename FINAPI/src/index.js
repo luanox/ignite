20a5f70b964a1b7447de7f9f0e613bb42f28b593
Luan Neves <luanuemg@hotmail.com>
2021-07-26 17:42:12 -0300

    feat(account): add customer already exists

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -1,13 +1,26 @@
-const express = require("express")
+const express = require('express')
+const {v4: uuidv4} = require('uuid')
 
 const app = express()
+app.use(express.json())
+const customers = []
 
-// app.json()
+app.post("/account",(req, res) => {
+  const {cpf, name} = req.body
 
-app.get("/", (req, res) => {
-    return res.json({message: "Hellooo"})
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if(customerAlreadyExists) return res.status(400).json({error: "Customer already exists!"})
+
+
+  customers.push({
+    id: uuidv4(),
+    name,
+    cpf,
+    statement: []
+  })
+
+  return res.status(201).json(customers)
 })
 
 app.listen(3333)
-
-console.log('Funcionou');
